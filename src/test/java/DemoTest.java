93370d3c0819ce24ebaaccec4295c0cfda8d3f6d
Gaurav <gauravmi@thoughtworks.com>
2020-02-11 23:39:50 +0530

    updating plugin description

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,13 +1,10 @@
-import com.sun.tools.javac.util.List;
 import org.junit.Test;
-
-import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
 public class DemoTest {
     @Test
     public void testMethod() {
-        assertEquals(1,1);
+        assertEquals(1, 1);
     }
 }
